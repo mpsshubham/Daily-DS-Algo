04df1f1890169bbb8adf2ab6b6b11b0af13b8a4c
EMP541 <shubham.sharma@finoit.co.in>
2022-11-03 21:59:39 +0530

    array

--- src/com/java/array/CheckArraySortedAndRotated03.java
+++ src/com/java/array/CheckArraySortedAndRotated03.java
@@ -1,2 +1,24 @@
-package src.com.java.array;public class CheckArraySortedAndRotated03 {
+package src.com.java.array;
+
+public class CheckArraySortedAndRotated03 {
+
+    public static void main(String[] args) {
+
+        int arr1[] =  {3,4,5,1,2};
+        System.out.println("Array is sorted and rotated: "+check(arr1));
+
+        int arr2[] =  {2,1,3,4};
+        System.out.println("Array is sorted and rotated: "+check(arr2));
+
+    }
+
+    static boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n])
+                count++;
+        }
+        return (count <= 1);
+    }
 }
--- src/com/java/array/RemoveDuplicatesFromSortedArray04.java
+++ src/com/java/array/RemoveDuplicatesFromSortedArray04.java
@@ -1,4 +1,30 @@
 package src.com.java.array;
 
 public class RemoveDuplicatesFromSortedArray04 {
+
+    public static void main(String[] args) {
+
+        int arr1[] =  {0,0,1,1,1,2,2,3,3,4};
+        System.out.println("Duplicates index: "+removeDuplicates(arr1,arr1.length));
+
+        int arr2[] =  {8,10,5,7,9};
+        System.out.println("Duplicates index: "+removeDuplicates(arr2,arr2.length));
+
+    }
+
+    static int removeDuplicates(int nums[],int size) {
+        int n = nums.length;
+        if (n == 0 || n == 1)
+            return n;
+
+        int j = 0;
+
+        for (int i = 0; i < n-1; i++)
+            if (nums[i] != nums[i+1])
+                nums[j++] = nums[i];
+
+        nums[j++] = nums[n-1];
+
+        return j;
+    }
 }
